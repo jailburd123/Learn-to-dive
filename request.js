05d7fda5269574db5fa137f3e806e3463dd6bb54
Joshua <jburden@aimcodeschool.org>
2022-08-12 01:28:08 -0700

    Too tired to fix async request issue

--- request.js
+++ request.js
@@ -12,3 +12,31 @@ console.log(numb2);
 console.log(numb1 + numb2);
 console.log(hello);
 console.log(hello + numb1);
+
+
+let getData = async () => {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=Omaha&APPID=fe2e25be076abfb7b948ebb4743d71d2')
+    .then((response) => 
+         response.json()
+    ).then((data) => 
+    console.log(data)
+    ).catch((err) => {
+        console.log('Error: ', err);
+    });
+  
+};
+
+
+// getData();
+   
+
+async function getWeatherData(url) {
+    const response = await fetch(url);
+  
+    return response.json();
+  }
+  
+  let url = 'https://api.openweathermap.org/data/2.5/weather?q=Omaha&APPID=fe2e25be076abfb7b948ebb4743d71d2';
+  const data = getWeatherData(url);
+  
+  console.log({ data })

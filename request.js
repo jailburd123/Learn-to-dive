f404b7464326bdf2dd97f1016b49be61bc3b1d9c
Joshua <jburden@aimcodeschool.org>
2022-08-11 21:14:39 -0700

    commit for tonights class

---
+++ request.js
@@ -0,0 +1,14 @@
+
+
+// set two variables to 5 and 6
+
+let numb1 = 5;
+let numb2 = 6;
+let hello = 'Hello World!';
+
+console.log(numb1);
+console.log(numb2);
+
+console.log(numb1 + numb2);
+console.log(hello);
+console.log(hello + numb1);

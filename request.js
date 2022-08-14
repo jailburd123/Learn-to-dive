b4a157b882fb95870f8e0eeaff585cef016f090b
Joshua <jburden@aimcodeschool.org>
2022-08-13 22:36:58 -0700

    commit getWeatherData

--- request.js
+++ request.js
@@ -14,29 +14,67 @@ console.log(hello);
 console.log(hello + numb1);
 
 
-let getData = async () => {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=Omaha&APPID=fe2e25be076abfb7b948ebb4743d71d2')
-    .then((response) => 
-         response.json()
-    ).then((data) => 
-    console.log(data)
-    ).catch((err) => {
-        console.log('Error: ', err);
-    });
+
+
+let convertToFariheit = (data) => {
+
+    let toF = ((data - 273.15) * 9/ 5) + 32;
+
+    console.log("new temp: ", toF);
+    return toF;
+    
+}
+
+
+let cardinalDirection = (data) => {
+    var val = Math.floor((data / 22.5) + 0.5);
+    var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+    let direction = arr[(val % 16)];
+    console.log(direction);
+    return direction;
+
+}   
+
   
+let weatherurl = 'https://api.openweathermap.org/data/2.5/weather?q=Omaha&APPID=fe2e25be076abfb7b948ebb4743d71d2';
+
+
+let getWeather = () => {
+    fetch(weatherurl)
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                return Promise.reject(response);
+            }
+        }).then((data) => {
+            console.log(data);
+            let cityName = data.name;
+            console.log("city Name: ", cityName);
+            let country = data.sys.country;
+            console.log("country: ", country);
+            let humidity = data.main['humidity'];
+            console.log("humidity: ", humidity);
+            let temp = data.main['temp'];
+            console.log("temp: ", temp);
+            let weatherdesc = data.weather[0].description;
+            console.log("description: ", weatherdesc);
+            let windSpeed = data.wind['speed'];
+            console.log('Wind Speed: ', windSpeed);
+            let windDirection = data.wind['deg'];
+            console.log('Wind direction: ', windDirection);
+
+
+            convertToFariheit(temp);
+            cardinalDirection(windDirection);
+
+    })
+        .catch((error) =>{
+            console.error(error);
+        }).finally(() => {
+        console.log('Successfully completed!')
+        });
+
 };
 
-
-// getData();
-   
-
-async function getWeatherData(url) {
-    const response = await fetch(url);
-  
-    return response.json();
-  }
-  
-  let url = 'https://api.openweathermap.org/data/2.5/weather?q=Omaha&APPID=fe2e25be076abfb7b948ebb4743d71d2';
-  const data = getWeatherData(url);
-  
-  console.log({ data })
+getWeather();
